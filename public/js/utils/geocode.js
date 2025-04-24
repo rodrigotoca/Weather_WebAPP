89282bdec16ab15896d5f518b0ce9dae2d4db314
Rodrigo Toca Sánchez <93493493+rodrigotoca@users.noreply.github.com>
2025-04-24 09:32:35 -0600

    Update geocode.js

--- public/js/utils/geocode.js
+++ public/js/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoicm9kcmlnb3RvY2EiLCJhIjoiY2t2OXZ1czViOHg4czJ3dDl2NmFjNXN0NCJ9.thqFauOpoUb_8jR8ARgecg&limit=1`
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${APITOKEN}&limit=1`
 
     request({url, json:true }, (error, {body}) => {
         if (error) {
